e7f0ef9b6a0ed3b05fee0f2390e4b55fa15b5a18
Azakahul <azakahul@gmail.com>
2019-02-24 16:35:57 -0600

    Added basic authorization to the get tasks api.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 tasks = [
     {
@@ -18,6 +19,18 @@ tasks = [
 
 app = Flask(__name__)
 
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 def make_public_task(task):
     new_task = {}
     for field in task:
@@ -32,6 +45,7 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
     return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
